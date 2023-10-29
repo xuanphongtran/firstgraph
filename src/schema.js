65e94b011068011a7fa35956fd15a9f2c060f7d3
phongbvb <phongbvb.09@gmail.com>
2023-10-29 22:53:43 +0700

    Initial commit

--- src/schema.js
+++ src/schema.js
@@ -1,4 +1,5 @@
 const { gql } = require('apollo-server')
+const { prisma } = require('./db')
 
 const typeDefs = gql`
   type Post {
@@ -18,57 +19,39 @@ const typeDefs = gql`
     publish(id: ID!): Post
   }
 `
-const posts = [
-  {
-    id: 1,
-    title: 'Subscribe to GraphQL Weekly for community news ',
-    content: 'https://graphqlweekly.com/',
-    published: true,
-  },
-  {
-    id: 2,
-    title: 'Follow DigitalOcean on Twitter',
-    content: 'https://twitter.com/digitalocean',
-    published: true,
-  },
-  {
-    id: 3,
-    title: 'What is GraphQL?',
-    content: 'GraphQL is a query language for APIs',
-    published: false,
-  },
-]
 
 const resolvers = {
   Query: {
     feed: (parent, args) => {
-      return posts.filter((post) => post.published)
+      return prisma.post.findMany({
+        where: { published: true },
+      })
     },
     post: (parent, args) => {
-      return posts.find((post) => post.id === Number(args.id))
+      return prisma.post.findUnique({
+        where: { id: Number(args.id) },
+      })
     },
   },
   Mutation: {
     createDraft: (parent, args) => {
-      posts.push({
-        id: posts.length + 1,
-        title: args.title,
-        content: args.content,
-        published: false,
+      return prisma.post.create({
+        data: {
+          title: args.title,
+          content: args.content,
+        },
       })
-      return posts[posts.length - 1]
     },
     publish: (parent, args) => {
-      const postToPublish = posts.find((post) => post.id === Number(args.id))
-      postToPublish.published = true
-      return postToPublish
+      return prisma.post.update({
+        where: {
+          id: Number(args.id),
+        },
+        data: {
+          published: true,
+        },
+      })
     },
-  },
-  Post: {
-    content: (parent) => parent.content,
-    id: (parent) => parent.id,
-    published: (parent) => parent.published,
-    title: (parent) => parent.title,
   },
 }
 
